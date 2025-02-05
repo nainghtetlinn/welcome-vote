0a232c2a222dcdb290fc4befab8944690d8904f8
nainghtetlinn <naing.work.it@gmail.com>
2025-02-05 18:31:24 +0630

    error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <div className='flex flex-col items-center justify-center gap-4 h-screen'>
+      <h3 className='text-lg'>Opps... Something went wrong.</h3>
+      <h1 className='font-bold text-3xl'>Internal Server Error: 500</h1>
+      <Button asChild>
+        <Link href='/'>Go Home</Link>
+      </Button>
+    </div>
+  )
 }
 
 export default Error
