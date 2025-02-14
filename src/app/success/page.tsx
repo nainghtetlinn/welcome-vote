73bf6d4258f63d0304e568341dffd4a9e5a6a7d8
nainghtetlinn <naing.work.it@gmail.com>
2025-02-14 22:41:47 +0630

    success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,11 @@
+const Success = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='font-bold text-3xl'>
+        Your votes are successfully submitted!
+      </h1>
+    </div>
+  )
+}
+
+export default Success
