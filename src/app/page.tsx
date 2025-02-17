1c1a8a941d0ae5537dc8bbd064eb3a912dd533e8
nainghtetlinn <naing.work.it@gmail.com>
2025-02-17 20:34:26 +0630

    redirect to active event when visiting "/"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <>Hello</>
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
+export default async function Home() {
+  const supabase = await createClient()
+  const event = await supabase
+    .from('events')
+    .select()
+    .eq('active', true)
+    .single()
+
+  if (event.error || !event.data) redirect('/error')
+
+  redirect('/event/' + event.data?.name)
 }
