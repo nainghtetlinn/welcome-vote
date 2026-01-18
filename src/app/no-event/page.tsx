5c26fb32e76d3f3c996fbf2d407030dfce72a5ec
nainghtetlinn <naing.work.it@gmail.com>
2026-01-18 19:19:24 +0630

    update icon in no-event page

--- src/app/no-event/page.tsx
+++ src/app/no-event/page.tsx
@@ -1,13 +1,13 @@
-import { CircleX } from "lucide-react";
+import { CircleCheck } from "lucide-react";
 
 const NoEvent = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="bg"></div>
       <div className="flex flex-col items-center gap-4 bg-white rounded-lg p-12 text-center">
-        <CircleX
+        <CircleCheck
           size={100}
-          className="text-red-500"
+          className="text-green-500"
         />
         <h1 className="font-bold text-3xl">Voting event has ended.</h1>
         <p>Results will be announced soon. Thank you for participating.</p>
